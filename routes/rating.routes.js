d0c101065444965d2b64577cccc0b81ab719705b
vuongphamavocado <vuongphamavocado@gmail.com>
2018-05-28 01:48:08 +0700

    Vuong pham update rating recommendation

--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -4,7 +4,7 @@ const router = new Router();
 const authMiddleWare = require('./../controllers/config/jwt');
 
 // Get Recommandation
-router.get('/:userId', authMiddleWare.verify, RatingController.getRecommendation);
+router.get('/', authMiddleWare.verify, RatingController.getRecommendation);
 
 // Put Rating
 router.post('/:hotelId/:ratingNumber', authMiddleWare.verify, RatingController.postRatingHotel);
